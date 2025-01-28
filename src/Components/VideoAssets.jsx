d488fe28165750737e734daddc360e3ae3aa3b26
Soban khan <rsoban22@gmail.com>
2025-01-28 19:14:37 +0500

    Soban

--- src/Components/VideoAssets.jsx
+++ src/Components/VideoAssets.jsx
@@ -5,6 +5,71 @@ const VideoAssets = () => {
   return (
     <div className="videos-main">
       <h1>Video Gallery</h1>
+
+      <div className='video-container'>
+
+      <iframe
+        width="600"
+        height="100"
+        src="https://youtube.com/embed/X-Qy1skMmm8?si=hXhS-3FlXkmuVTqV"
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+
+      <iframe
+        width="600"
+        height="100"
+        src="https://youtube.com/embed/vb9kEctrLkk"
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+
+
+      <iframe
+        width="600"
+        height="100"
+        src="https://youtube.com/embed/JOqlbOruMAk?si=3jm6PXUFL85EsxAO"
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+    
+      </div>
+
+
+      <div className='video-container'>
+
+      <iframe
+        width="600"
+        height="100"
+        src="https://youtube.com/embed/WZNuGuin-xA"
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+
+      <iframe
+        width="600"
+        height="100"
+        src="https://youtube.com/embed/D_LMF02oKdg?si=i-GmGr6yrKLLsI6I"
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+
+      <iframe
+        width="600"
+        height="100"
+        src="https://youtube.com/embed/qOBDfEorfpc"
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+    
+      </div>
+
       <div className='video-container'>
 
       <iframe
@@ -28,44 +93,17 @@ const VideoAssets = () => {
       <iframe
         width="600"
         height="100"
-        src="https://youtube.com/embed/vb9kEctrLkk"
-        title="YouTube video player"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      ></iframe>
-    
-      </div>
-
-      <div className='video-container'>
-
-      <iframe
-        width="600"
-        height="100"
-        src="https://youtube.com/embed/WZNuGuin-xA"
-        title="YouTube video player"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      ></iframe>
-
-      <iframe
-        width="600"
-        height="100"
         src="https://youtube.com/embed/VylCtQnskkE"
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
       ></iframe>
-
-      <iframe
-        width="600"
-        height="100"
-        src="https://youtube.com/embed/qOBDfEorfpc"
-        title="YouTube video player"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      ></iframe>
     
       </div>
+
+      
+
+      
 
     </div>
   );
